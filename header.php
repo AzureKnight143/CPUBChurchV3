c1c6ca7879562f32d0724323f19bb28406712428
Matt Wright <azureknight143@gmail.com>
2018-04-24 20:21:42 -0400

    Styled mobile navigation

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 
 		<div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
 			<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
-			<nav class="navbar navbar-expand-md navbar-dark">
+			<nav class="navbar navbar-expand-lg navbar-dark">
 			<?php if ( 'container' == $container ) : ?><div class="container"><?php endif; ?>
 			
 				<h1 class="navbar-brand mb-0 d-flex align-items-center">
@@ -31,29 +31,29 @@
 					</a>	
 				</h1>																	
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<div class="d-flex align-items-end flex-column">
-					<div class="d-flex">
-					<?php wp_nav_menu(
+				<div class="d-flex align-items-lg-end align-items-md-start flex-column w-100">
+					<div class="d-flex align-items-center justify-content-end w-100">
+						<?php wp_nav_menu(
 							array(
 								'theme_location'  => 'secondary',
-								'container_class' => 'd-none d-md-block',
+								'container_class' => 'd-none d-lg-block',
 								'menu_class'      => 'navbar-nav',
 								'menu_id'         => 'secondary-menu',
 								'walker'          => new understrap_WP_Bootstrap_Navwalker(),
 							)
 						); ?>
 
-						<div class="social">
+						<div class="social ml-lg-2 mr-3">
 							<a href="https://www.facebook.com/cpubchurch" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
 							<a href="https://twitter.com/cpubchurch" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
 							<a href="<?php echo esc_url( home_url('feed') ); ?>" target="_blank"><i class="fa fa-rss" aria-hidden="true"></i></a>
 						</div>
 
 						<?php get_search_form(true) ?>
+
+						<button class="navbar-toggler ml-lg-3 ml-auto" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
 					</div>
 
 					<div id="navbarNavDropdown" class="collapse navbar-collapse">
@@ -69,7 +69,7 @@
 						<?php wp_nav_menu(
 							array(
 								'theme_location'  => 'secondary',
-								'container_class' => 'd-md-none',
+								'container_class' => 'd-lg-none',
 								'menu_class'      => 'navbar-nav',
 								'menu_id'         => 'secondary-menu',
 								'walker'          => new understrap_WP_Bootstrap_Navwalker(),
