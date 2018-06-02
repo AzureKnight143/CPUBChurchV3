4f04ea38607548fea90b036bc9923a0c77815dfa
Matt Wright <azureknight143@gmail.com>
2018-06-02 15:27:02 -0400

    Cleaned up page posts.

--- page-templates/sections/posts.php
+++ page-templates/sections/posts.php
@@ -1,23 +1,27 @@
 <?php
 $categoryMeta = get_post_meta( get_the_ID(), 'category', true );
-$category = get_category_by_slug( $categoryMeta );
-$categoryLink = get_category_link( $category->cat_ID );
 
-$category_posts_args = array(
-    'post_status' => 'publish',
-    'posts_per_page' => 5,
-    'category_name' => $category->slug);
-$category_posts = new WP_Query( $category_posts_args );
+if ($categoryMeta) :
+    $category = get_category_by_slug( $categoryMeta );
+    $categoryLink = get_category_link( $category->cat_ID );
 
-if ( $category_posts->have_posts() ) :
-    while ( $category_posts->have_posts() ) : $category_posts->the_post();
-        get_template_part( 'loop-templates/content', get_post_format() );
-    endwhile;   
-    wp_reset_postdata();
+    $category_posts_args = array(
+        'post_status' => 'publish',
+        'posts_per_page' => 5,
+        'category_name' => $category->slug);
+    $category_posts = new WP_Query( $category_posts_args );
 ?>
-    <a class="btn btn-outline-primary" href="<?php echo $categoryLink ?>">Read Older Posts</a>
-<?php
-else :
-    get_template_part( 'loop-templates/content', 'none' );
-endif;
-?>
+
+    <?php if ( $category_posts->have_posts() ) : ?>
+        <div class="page-posts">
+            <h1>Latest News</h2>
+            <?php
+            while ( $category_posts->have_posts() ) : $category_posts->the_post();
+                get_template_part( 'loop-templates/content', get_post_format() );
+            endwhile;   
+            wp_reset_postdata();
+            ?>
+            <a class="btn btn-outline-primary" href="<?php echo $categoryLink ?>">Read Older Posts</a>
+        </div>
+    <?php endif; ?>
+<?php endif; ?>
