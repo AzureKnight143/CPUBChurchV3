961122ce2adf0da18861536fcfaf44f626ce5120
Matt Wright <azureknight143@gmail.com>
2021-11-19 10:19:18 -0500

    redisgned header

--- footer.php
+++ footer.php
@@ -1,15 +1,18 @@
             <?php
-                $the_theme = wp_get_theme();
-                $container = get_theme_mod( 'understrap_container_type' );
+            $the_theme = wp_get_theme();
+            $container = get_theme_mod('understrap_container_type');
             ?>
 
-            <?php get_sidebar( 'footerfull' ); ?>
-
             <div class="wrapper" id="wrapper-footer">
-                <div class="<?php echo esc_attr( $container ); ?>">
+                <div class="<?php echo esc_attr($container); ?>">
                     <div class="row">
                         <div class="col-md-12">
                             <footer class="site-footer" id="colophon">
+                                <div class="social ml-lg-2 mr-3">
+                                    <a href="https://www.facebook.com/cpubchurch" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="https://twitter.com/cpubchurch" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    <a href="<?php echo esc_url(home_url('newsletters')); ?>"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+                                </div>
                                 <div class="site-info">
                                     1945 College Ave., Huntington, IN 46750 <span class="pipe">|</span> (260)&nbsp;356&#8209;2642 <span class="pipe">|</span> <a href="https://goo.gl/maps/xZGMzXRckbo" target="_blank">Map&nbsp;&amp;&nbsp;Directions</a>
                                     <br />
@@ -20,7 +23,8 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <?php wp_footer(); ?>
-    </body>
-</html>
+            </div>
+            <?php wp_footer(); ?>
+            </body>
+
+            </html>
--- header.php
+++ header.php
@@ -1,87 +1,41 @@
-<?php $container = get_theme_mod( 'understrap_container_type' ); ?>
+<?php $container = get_theme_mod('understrap_container_type'); ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<?php wp_head(); ?>
-	</head>
 
-	<body <?php body_class(); ?>>
-		<div class="hfeed site" id="page">
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<?php wp_head(); ?>
+</head>
 
-			<div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
-				<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
-				<nav class="navbar navbar-expand-lg navbar-dark">
-				<?php if ( 'container' == $container ) : ?><div class="container"><?php endif; ?>
-				
-					<h1 class="navbar-brand mb-0 d-flex align-items-center">
-						<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
-							<img class="logo" src="<?php echo dirname( get_bloginfo('stylesheet_url')); ?>/images/logo.png" alt="College Park Church" />
-						</a>
-						<a class="ml-2" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
-							<span class="brand-bold">College Park</span>
-							<span class="brand-spaced">Church</span>
-						</a>	
-					</h1>																	
-
-					<div class="d-flex align-items-lg-end align-items-md-start flex-column w-100">
-						<div class="d-flex align-items-center justify-content-end w-100">
-							<?php wp_nav_menu(
-								array(
-									'theme_location'  => 'secondary',
-									'container_class' => 'd-none d-lg-block',
-									'menu_class'      => 'navbar-nav',
-									'menu_id'         => 'secondary-menu',
-									'walker'          => new understrap_WP_Bootstrap_Navwalker(),
-								)
-							); ?>
-
-							<div class="social ml-lg-2 mr-3">
-								<a href="https://www.facebook.com/cpubchurch" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-								<a href="https://twitter.com/cpubchurch" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-								<a href="<?php echo esc_url( home_url('newsletters') ); ?>"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
-							</div>
-
-							<?php get_search_form(true) ?>
-
-							<button class="navbar-toggler ml-lg-3 ml-auto" type="button" data-toggle="collapse" data-target="#navbar-dropdown" aria-controls="navbar-dropdown" aria-expanded="false" aria-label="Toggle navigation">
-								<span class="navbar-toggler-icon"></span>
-							</button>
-						</div>
-
-						<div id="navbar-dropdown" class="collapse navbar-collapse mt-lg-3 mt-md-3 pt-md-2">
-							<span class="navbar-text">Connect with</span>
-							<i class="navbar-text arrow fa fa-angle-right" aria-hidden="true"></i>
-
-							<?php wp_nav_menu(
-								array(
-									'theme_location'  => 'primary',
-									'menu_class'      => 'navbar-nav ml-4 ml-lg-0',
-									'menu_id'         => 'main-menu',
-									'walker'          => new understrap_WP_Bootstrap_Navwalker(),
-									"depth" 		  => 2
-								)
-							); ?>
-
-							<?php wp_nav_menu(
-								array(
-									'theme_location'  => 'secondary',
-									'container_class' => 'd-lg-none',
-									'menu_class'      => 'navbar-nav',
-									'menu_id'         => 'secondary-menu',
-									'walker'          => new understrap_WP_Bootstrap_Navwalker(),
-								)
-							); ?>
-						</div>
+<body <?php body_class(); ?>>
+	<div class="hfeed site" id="page">
+		<div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
+			<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e('Skip to content', 'understrap'); ?></a>
+			<nav class="navbar navbar-expand-lg navbar-dark">
+				<?php if ('container' == $container) : ?><div class="container"><?php endif; ?>
+					<a rel="home" href="<?php echo esc_url(home_url('/')); ?>" itemprop="url">
+						<img class="logo-mobile d-lg-none" src="<?php echo dirname(get_bloginfo('stylesheet_url')); ?>/images/logo.png" alt="College Park Church" />
+					</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-dropdown" aria-controls="navbar-dropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div id="navbar-dropdown" class="collapse navbar-collapse">
+						<?php wp_nav_menu(array(
+							'theme_location'  => 'primary',
+							'menu_class'      => 'navbar-nav',
+							'menu_id'         => 'main-menu',
+							'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+							"depth" 		  => 2
+						)); ?>
 					</div>
-
-					<?php if ( 'container' == $container ) : ?></div><?php endif; ?>
-				</nav>
-			</div>
+					<?php if ('container' == $container) : ?>
+					</div><?php endif; ?>
+			</nav>
+		</div>
