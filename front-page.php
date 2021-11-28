66c6ab90840b5a25bd47b62efed40b46e7af508b
Matt Wright <azureknight143@gmail.com>
2021-11-28 13:39:05 -0500

    Added home page banner Added contact info to footer

--- front-page.php
+++ front-page.php
@@ -6,28 +6,47 @@ get_header();
 <div class="wrapper" id="home-wrapper">
     <main class="site-main" id="main">
         <div class="<?php echo esc_attr($container); ?>">
+            <div class="banner">
+                <?php if (get_theme_mod('banner_title')) { ?>
+                    <h1><?php echo get_theme_mod('banner_title') ?></h1>
+                <?php } ?>
+                <?php if (get_theme_mod('banner_subtitle')) { ?>
+                    <h2><?php echo get_theme_mod('banner_subtitle') ?></h2>
+                <?php } ?>
+                <?php if (get_theme_mod('banner_content')) { ?>
+                    <p><?php echo get_theme_mod('banner_content') ?></p>
+                <?php } ?>
+                <div class="links">
+                    <?php if (get_theme_mod('banner_link_1') && get_theme_mod('banner_link_text_1')) { ?>
+                        <a href="<?php echo get_permalink(get_theme_mod('banner_link_1')) ?>"><?php echo get_theme_mod('banner_link_text_1') ?></a>
+                    <?php } ?>
+                    <?php if (get_theme_mod('banner_link_2') && get_theme_mod('banner_link_text_2')) { ?>
+                        <a href="<?php echo get_permalink(get_theme_mod('banner_link_2')) ?>"><?php echo get_theme_mod('banner_link_text_2') ?></a>
+                    <?php } ?>
+                </div>
+            </div>
             <div class="highlights">
-                <?php if (get_theme_mod('highlight_main_text_1') && get_theme_mod('highlight_link_1')) { ?>
+                <?php if (get_theme_mod('highlight_title_1') && get_theme_mod('highlight_link_1')) { ?>
                     <a class="highlight" href="<?php echo get_permalink(get_theme_mod('highlight_link_1')) ?>">
-                        <h2><?php echo get_theme_mod('highlight_main_text_1') ?></h2>
-                        <?php if (get_theme_mod('highlight_secondary_text_1')) { ?>
-                            <p><?php echo get_theme_mod('highlight_secondary_text_1') ?></p>
+                        <h2><?php echo get_theme_mod('highlight_title_1') ?></h2>
+                        <?php if (get_theme_mod('highlight_subtitle_1')) { ?>
+                            <p><?php echo get_theme_mod('highlight_subtitle_1') ?></p>
                         <?php } ?>
                     </a>
                 <?php } ?>
-                <?php if (get_theme_mod('highlight_main_text_2') && get_theme_mod('highlight_link_2')) { ?>
+                <?php if (get_theme_mod('highlight_title_2') && get_theme_mod('highlight_link_2')) { ?>
                     <a class="highlight" href="<?php echo get_permalink(get_theme_mod('highlight_link_2')) ?>">
-                        <h2><?php echo get_theme_mod('highlight_main_text_2') ?></h2>
-                        <?php if (get_theme_mod('highlight_secondary_text_2')) { ?>
-                            <p><?php echo get_theme_mod('highlight_secondary_text_2') ?></p>
+                        <h2><?php echo get_theme_mod('highlight_title_2') ?></h2>
+                        <?php if (get_theme_mod('highlight_subtitle_2')) { ?>
+                            <p><?php echo get_theme_mod('highlight_subtitle_2') ?></p>
                         <?php } ?>
                     </a>
                 <?php } ?>
-                <?php if (get_theme_mod('highlight_main_text_3') && get_theme_mod('highlight_link_3')) { ?>
+                <?php if (get_theme_mod('highlight_title_3') && get_theme_mod('highlight_link_3')) { ?>
                     <a class="highlight" href="<?php echo get_permalink(get_theme_mod('highlight_link_3')) ?>">
-                        <h2><?php echo get_theme_mod('highlight_main_text_3') ?></h2>
-                        <?php if (get_theme_mod('highlight_secondary_text_3')) { ?>
-                            <p><?php echo get_theme_mod('highlight_secondary_text_3') ?></p>
+                        <h2><?php echo get_theme_mod('highlight_title_3') ?></h2>
+                        <?php if (get_theme_mod('highlight_subtitle_3')) { ?>
+                            <p><?php echo get_theme_mod('highlight_subtitle_3') ?></p>
                         <?php } ?>
                     </a>
                 <?php } ?>
